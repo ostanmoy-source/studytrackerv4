00e17e3ecd7ef70cc546728005833708a029bfdc
ostanmoy-source <ostanmoy@gmail.com>
2026-02-24 04:20:19 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,11 @@
-const FIREBASE_CONFIG = {
-  apiKey:            "AIzaSyDlTlxoLtf8FaM-1RxGh1_v0VqvLwVBKSI",
-  authDomain:        "mo-tanmoy.firebaseapp.com",
-  databaseURL:       "https://mo-tanmoy-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId:         "mo-tanmoy",
-  storageBucket:     "mo-tanmoy.firebasestorage.app",
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDlTlxoLtf8FaM-1RxGh1_v0VqvLwVBKSI",
+  authDomain: "mo-tanmoy.firebaseapp.com",
+  databaseURL: "https://mo-tanmoy-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "mo-tanmoy",
+  storageBucket: "mo-tanmoy.firebasestorage.app",
   messagingSenderId: "654071941987",
-  appId:             "1:654071941987:web:36c6f93d397573eefd6156"
+  appId: "1:654071941987:web:36c6f93d397573eefd6156",
+  measurementId: "G-M4MQFN2MWE"
 };
-
-const ADMIN_EMAILS = [
-  "ostanmoy@gmail.com"
-];
